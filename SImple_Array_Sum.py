f08929312716a58d51952d384a634d865ab65622
Shrishti Hore <40817931+ShrishtiHore@users.noreply.github.com>
2020-06-23 20:23:08 +0530

    Create SImple_Array_Sum.py

---
+++ SImple_Array_Sum.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import os
+import sys
+
+def simpleArraySum(ar):
+    return(sum(ar))
+
+#driver function
+ar = []
+
+ar_count = len(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
